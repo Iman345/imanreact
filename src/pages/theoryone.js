9cac8de5932d95664dd01aa9aedae9d539f4a520
Nami <60913360+Iman345@users.noreply.github.com>
2024-05-20 11:02:47 +0200

    Update theoryone.js

--- src/pages/theoryone.js
+++ src/pages/theoryone.js
@@ -41,65 +41,6 @@ function Theoryone() {
 
 
 
-                    <p>
-                        The neoliberalist nature of our society has granted monstrous power to the private sector (Giroux, 2005). Within the private sector one of the most powerful entities are technological corporations who Abeba Birhane (2020) argues “hold unprecedented power and influence.” The technological giants with unprecedent power and influence all belong to the West. Since technology is influential in every aspect of our society, the West holds power and influence over every aspect of our society. This phenomenon creates digital coloniality which is rampant in the global South. Birhane (2020) explores how the West sustains their coloniality through Algorithmic Colonization in their paper <i>Algorithmic Colonization of Africa </i> which will be the focal point of this essay.
-
-                    </p>
-
-                    <p>
-                        Technology can advance our lifestyles and assist us in every possible field, it can help us connect to people, find information, advance medical research etc. The possibilities for technology to aid and grow society are limitless but the business model and profit margins that technology companies chase can cause more harm than good. Big technological companies put profit before people and their practises often go unquestioned. In their paper, Birhane does not advocate against using Western technology, automated systems and AI but rather asks us to examine them.
-                    </p>
-
-                    <p>
-                        Traditional colonialism may be thought of as a physical invasion where brute force is enlisted to gain control of political, economic, and social authority and influence. Digital colonialism and algorithmic colonialism hope to achieve the same control but through software and AI without the explicit brute force. Therefore, while traditional colonization is different to algorithmic colonization in how it is implemented and executed their goals are inline, to assert dominance in social, cultural, and political spheres by gaining control of crucial systems central to running countries and generating wealth.
-
-                    </p>
-                    <p>
-
-                        Digital and traditional colonization alike, use government, politics and wealth to sustain power using these systems in unison. Algorithmic colonization is present in the Global South through the dependency the Global South has on “Western software and infrastructure” (Birhane 2020). Western software and infrastructure are bias because they are created for and by the West, thus does not cater to the African countries and their needs. In their paper Birhane challenges “the relevance and appropriateness of AI software developed with values, norms, and interests of Western societies to that of users across the African continent”. Birhane makes the important point in his statement that relying on Western software and infrastructure is not ideal and can be damaging to the Global South because the cultures, value systems, social, political and economic systems are vastly different. With completely different priorities Birhane argues that Western software can often be more hindering than helpful. Birhane gives the example of medical software that is geared towards Western countries being implemented in Africa. The software will therefore not account for the different treatments and different medical conditions of the African country.  This illustrates that Western solutions can be more damaging to African countries than helpful because they do not take context into account.
-                    </p>
-                    <p>
-
-                        Dependency exclusively on the West, additionally leaves no space for local software and technology to be implemented giving the West an unchallenged space to control the global South through the technology sector. There are huge technological hubs all over Africa but none of them flourish at the level at the same level that Western companies and technologies do because most African companies and governments import technology and software instead of turning to local solutions.
-                    </p>
-
-                    <p>
-                        Furthermore, Birhane discusses the danger of not questioning AI and technology and the dehumanizing nature of looking to AI to “fix” systematic and historical social issues. Birhane argues that societal issues cannot be quantified into a box that can be easily fixed using technology. Especially considering that technology is not neutral. Automated systems and AI solutions are often thought of as “neutral” and “objective” because technology is man-made it cannot have feelings or bias. However, because it is man-made technology carries the bias and ideals of its makers. Therefore, it can never be objective.  For instance, Western technology inherent Western thinking. When Western social, political and cultural ideologies are present it can be immensely dangerous. For example, GPT-3: OpenAI’s Million Dollar Language Model is inherently racist and sexist. It is a powerful machine learning tool that can mimic human like text producing poetry and even html code, for example it can take a prompt as input and output a completed sentence or sentences from the prompt. While it is powerful, it is also incredibly biased and prejudice. While language models can help create solutions to analysing data and optimizing systems etc, it can also be severely misused. For example, when typing in a sentence about Muslims as an input, the output received from GPT-3 revolves around violence, murder, and terrorism exclusively. This demonstrates the power and the terror of AI that uses Western Algorithms only. The creators of GPT-3 are aware of the racial and sexist nature of the AI and are researching on how to solve the issues, OpenAi still commercialized the software.
-
-                    </p>
-                    <p>
-                        Here are the examples of GPT3 and some of the racist and sexist outputs it generates.
-                    </p>
-                    <div className='imgs'>
-                    <img src={image5} alt="OpenApi Tweet by Abubakr Abid" className="screenshot" width="400" height="300"/>
-                    <img src={image4} alt="OpenApi Tweet by Abubakr Abidt" className="screenshot"width="400" height="200" />
-                    <img src={image1} alt="OpenApi Tweet by Kate Devlin" className="screenshot" width="400" height="200"/>
-                  
-                    <img src={image3} alt="OpenApi Tweet by Kate Devlin" className="screenshot" width="400" height="400"/>
-                    <img src={image2} alt="OpenApi Tweet by Jerome Pesenti" className="screenshot" width="400" height="200"/>
-                    
-                    </div>
-                   
-                    <p>
-
-                        Ethics are often undermined when it comes to algorithms because they are thought of as neutral. The European Digital Rights organisation argues that “Policymakers must be aware of the political, social and economic consequences of ‘solutions’ that concentrate power in the hands of technology companies”. Birhane (2020) discusses the importance of ethics in algorithms and how it is important to have a board who reviews the ethics of algorithms especially having a local board because technology firms often go unchecked. Having a local board to ethically analyse technology will allow for context of that country and society to be considered instead of a one size fits all ethical board making decisions that might not benefit all countries and communities.  Ethical review boards can help eliminate and reduce the discrimination that marginalized people can face by automated systems such as facial recognition and biometric systems. Ethics boards also help ensure that the security and privacy of people is upheld. However, ethics board rarely are concerned with how technology and algorithms will affect those who are marginalized and discriminated against and are often centred around what is profitable and what “fixes” more societal issues. This is harmful because it is quantifying people into statistics and reducing complex societal issues into black and white problems that can be solved while subsequently ignoring the needs of marginalized people. The unethical ethical boards just deepen societal issues while generating profit for the already rich.
-                    </p>
-
-
-                    <p>
-                        Africa is a continent where “community values triumph and nobody is left behind” (Birhane 2020). However, Western countries ideals are significantly different centred around thriving off rugged individualism. Based on the difference in ideals alone, Western technology is not suited to fulfil African ideals because it is built to fulfil Western capitalist ideals. Therefor it is urgent that African developers and innovators be the creators of the software and technological infrastructures implemented in African countries. Only then can Africa begin to break away from the coloniality sustained through technology.
-                    </p>
-
-
-                    <h3 className='page-header'>References</h3>
-
-                    <ul className='references'>
-
-                        <li>
-                            Birhane, A. (2020) Algorithmic Colonization of Africa.
-
-
-                        </li>
                         <li>
                             Giroux, H. (2005). The Terror of Neoliberalism: Rethinking the Significance of Cultural Politics. College Literature.
                         </li>
